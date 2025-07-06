9885c888d79c1b84e2cb5d9b9c731b3a6c599917
Eamon Burns <eamon.r.burns@gmail.com>
2025-07-05 23:30:22 -0700

    feat: refactor `on_message` command into app command

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,20 +1,26 @@
 import discord
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
+from discord import app_commands
 
 
-@client.event
-async def on_ready():
-    print(f"We have logged in as {client.user}")
+def init_client(guild_id: int) -> discord.Client:
+    intents = discord.Intents.default()
+    intents.message_content = True
 
+    client = discord.Client(intents=intents)
 
-@client.event
-async def on_message(message: discord.Message):
-    if message.author == client.user:
-        return
+    tree = app_commands.CommandTree(client)
 
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+    @tree.command(
+        name="hello",
+        description="A greeting",
+        guild=discord.Object(id=guild_id),
+    )
+    async def hello(interaction: discord.Interaction):
+        await interaction.response.send_message("Hello!")
+
+    @client.event
+    async def on_ready():
+        await tree.sync(guild=discord.Object(id=guild_id))
+        print(f"We have logged in as {client.user}")
+
+    return client
--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 import os
+from typing import TypeVar
 
 from dotenv import load_dotenv
 
 import bot
+
+T = TypeVar("T")
+
+
+def _get_env(t: T, name: str) -> T | None:
+    val_str = os.environ.get(name)
+    if val_str is None:
+        return None
+
+    try:
+        val = t(val_str)
+        return val
+    except ValueError as e:
+        print(e)
+        return None
 
 
 if __name__ == "__main__":
     # Get configuration from environment
     load_dotenv()
 
-    BOT_TOKEN = os.environ.get("DISCORD_TOKEN")
+    BOT_TOKEN = _get_env(str, "DISCORD_TOKEN")
     if BOT_TOKEN is None:
-        print("Error: `BOT_TOKEN` must be defined")
+        print("Error: `DISCORD_TOKEN` must be defined")
         exit(1)
 
-    bot.client.run(BOT_TOKEN)
+    GUILD_ID = _get_env(int, "DISCORD_GUILD")
+    if GUILD_ID is None:
+        print("Error: `DISCORD_GUILD` must be defined")
+        exit(1)
+
+    # Create and run the client
+    client = bot.init_client(GUILD_ID)
+
+    client.run(BOT_TOKEN)
